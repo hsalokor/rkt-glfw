854c9d022edeaa941e82518888268cb7d306ef60
Harri Salokorpi <hsalokor@iki.fi>
2012-11-26 00:49:09 +0200

    Refactor tests, add smoke tests for window open (fullscreen/windowed) intended to catch broken and crashing bindings

--- glfw-test.rkt
+++ glfw-test.rkt
@@ -3,13 +3,28 @@
 (require (planet schematics/schemeunit:3:4/text-ui))
 (require "glfw.rkt")
 
-(run-tests
-   (test-suite "Initialization and metadata"
-      #:before (lambda () (glfwInit))
-      #:after (lambda () (glfwTerminate)) 
+(define-test-suite glfw-init
+  #:before (lambda () (glfwInit))
+  #:after (lambda () (glfwTerminate)) 
 
-      (test-case "glfwGetVersion returns valid version information"
-                 (let-values ([(major minor rev) (glfwGetVersion)]) 
-                   (check-not-eq? major 0)
-                   (check-not-eq? minor 0)
-                   (check-not-eq? rev 0)))))
+  (test-case "Version query succeeds with non-zero values"
+             (let-values ([(major minor rev) (glfwGetVersion)]) 
+               (check-not-eq? major 0)
+               (check-not-eq? minor 0)
+               (check-not-eq? rev 0)))
+
+  (test-case "Opening window in windowed mode succeeds"
+             (glfwOpenWindow 400 300
+                             8 8 8
+                             0 0 0
+                             GLFW_WINDOW)
+             (glfwCloseWindow))
+
+  (test-case "Opening window in fullscreen mode succeeds"
+             (glfwOpenWindow 640 480
+                             8 8 8
+                             0 0 0
+                             GLFW_FULLSCREEN)
+             (glfwCloseWindow)))
+
+(run-tests glfw-init)
