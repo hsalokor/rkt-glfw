41a724132c2a537cb02e221e83fb587783285d3c
Kevin Robert Stravers <macocio@gmail.com>
2017-03-03 20:07:28 +0100

    Implement the entire GLFW3 API
    This API is generated by pruning everything from the glfw3.h file. It's currently untested and the three vulkan calls I'm not so sure about.
    
    The problem with the vulkan calls is that they use structures that are not defined, and I do not know if they are pointers.
    
    For now, I've made them pointers, since the docs say that you can pass in NULL.
    
    One thing that should be done is to rename all outputs (automatically of course) so that they are of the form glfw:Name, but it's not that much of a problem.
    
    Thanks to lexi-lambda and notjack from the racket IRC for the macro help!

--- example.rkt
+++ example.rkt
@@ -1,18 +1,10 @@
-#lang racket/base
+#! /usr/bin/env racket
+#lang racket
 
 (require "glfw.rkt")
 
-(define window-width 800)
-(define window-height 600)
-
-(define (init)
-  (begin
-    (glfwInit)
-    (glfwOpenWindow window-width window-height
-                    8 8 8
-                    0 0 0
-                    GLFW_WINDOW)
-    (glfwSetWindowTitle "Lolbal")
-    (sleep 4)
-    (glfwCloseWindow)
-    (glfwTerminate)))
+(glfwInit)
+(define window (glfwCreateWindow 800 600 "Example window" #f #f))
+(sleep 3)
+(glfwDestroyWindow window)
+(glfwTerminate)
